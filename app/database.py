b2dbb96f225b2165534d06c466d8ee12ca1b6ddd
Francois Pieterse <100724847+PieterseFrancois@users.noreply.github.com>
2024-09-16 23:32:52 +0200

    Initial architecture setup

---
+++ app/database.py
@@ -0,0 +1,27 @@
+from decouple import config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import registry, sessionmaker
+from sqlalchemy_utils import database_exists, create_database
+
+# Load database URL from environment variables
+DATABASE_URL = config("DATABASE_URL")
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+# Create a base class for declarative class definitions
+mapper_registry = registry()
+Base = mapper_registry.generate_base()
+
+# Create a configured "Session" class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db() -> None:
+    """
+    Initializes the database by creating tables.
+    Checks if the database exists and creates it if it doesn't.
+    """
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
+    Base.metadata.create_all(bind=engine)
