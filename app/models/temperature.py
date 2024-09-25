00b1c4f13f4ba8e73850d3f4699b250e4782dce1
Francois Pieterse <100724847+PieterseFrancois@users.noreply.github.com>
2024-09-25 20:49:38 +0200

    Added model for the temperature log table.

---
+++ app/models/temperature.py
@@ -0,0 +1,24 @@
+from app.database import Base
+
+from sqlalchemy import Column, Integer, Float, DateTime
+from datetime import datetime
+
+
+class TemperatureLogs(Base):
+    """
+    Represents the temperature logs table.
+
+    Attributes:
+        id (int): The primary key of the table.
+        temperature (float): The temperature value.
+        created_at (datetime): The timestamp when the log was created
+
+    Table Name:
+        temperature_logs
+    """
+
+    __tablename__ = "temperature_logs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    temperature = Column(Float, nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(datetime.timezone.utc))
