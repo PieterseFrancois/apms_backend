1b37bccb9aa4cbaa26d6c663c060c1e4aa92e220
Francois Pieterse <100724847+PieterseFrancois@users.noreply.github.com>
2024-10-13 13:02:07 +0200

    Add message identifiers for MQTT and websocket messages.

---
+++ app/utils/message_identifiers.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+
+class MessageIdentifiers(Enum):
+    """
+    Represents the message identifiers associated with the MQTT and websocket messages.
+    """
+
+    # Oven
+    BakeBatch = 'BakeBatch'
+    StopBake = 'StopBake'
+    EHeat = 'EHeat'
+    EFan = 'EFan'
+    BakeSafe = 'BakeSafe'
+    BakeOverride = 'BakeOverride'
+    SafeTemp = 'SafeTemp'
+    DesiredTemp = 'DesiredTemp'
+    MaxTemp = 'MaxTemp'
+    BakeTime = 'BakeTime'
+    Humidity = 'Humidity'
+    CurrentTemp = 'CurrentTemp'
+    BakePhase = 'BakePhase'
+    
+    # Press
+    Pressbatch = 'Pressbatch'
+    StopPress = 'StopPress'
+    OpenPress = 'OpenPress'
+    ConfirmInserted= 'ConfIns'
+    EPress = 'EPress'
+    ELoad = 'ELoad'
+    PressSafe = 'PressSafe'
+    PressOverride = 'PressOverride'
+    CurrentDistance = 'CurrentDist'
+    PressPhase = 'PressPhase'
+
+    def __str__(self) -> str:
+        return self.value
