82400560ceb8b7b83958d7d23e773a7ee4ff1e52
Francois Pieterse <100724847+PieterseFrancois@users.noreply.github.com>
2024-10-14 20:30:57 +0200

    Add machine schemas to __init__.py file.

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -6,7 +6,19 @@ from app.schemas.temperature import (
     TemperatureLogBase,
 )
 
+from app.schemas.machine import (
+    MachineBase,
+    Machine,
+    MachineCreate,
+    MachineUpdate,
+)
+
+
 __all__ = [
     "Response",
+    "MachineBase",
+    "Machine",
+    "MachineCreate",
+    "MachineUpdate",
     "TemperatureLogBase",
 ]
