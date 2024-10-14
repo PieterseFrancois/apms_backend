5cec24c08c0ccd95d9cdea012867f2ed60d1b76d
Francois Pieterse <100724847+PieterseFrancois@users.noreply.github.com>
2024-10-14 20:28:37 +0200

    Updated Response schema to include Machine schemas.

--- app/schemas/response.py
+++ app/schemas/response.py
@@ -3,6 +3,12 @@ from pydantic import BaseModel
 from app.utils.http_messages import HTTPMessages
 
 from app.schemas.temperature import TemperatureLogBase
+from app.schemas.machine import (
+    Machine,
+    MachineBase,
+    MachineCreate,
+    MachineUpdate,
+)
 
 
 class Response(BaseModel):
@@ -13,11 +19,18 @@ class Response(BaseModel):
         success (bool): Indicates the success status of the API response.
         msg (HTTPMessages): The HTTP message associated with the API response.
         data (Union[
-            list[TemperatureLogBase],
+            list[],
         ]): The list of data returned by the API response.
 
     """
 
     success: bool
     msg: HTTPMessages
-    data: Union[list[TemperatureLogBase], None]
+    data: Union[
+        list[TemperatureLogBase],
+        list[Machine],
+        list[MachineBase],
+        list[MachineCreate],
+        list[MachineUpdate],
+        None,
+    ]
