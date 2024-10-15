1731657e9338ddb879edde3fc1fcd620281c520e
Francois Pieterse <100724847+PieterseFrancois@users.noreply.github.com>
2024-10-15 09:58:03 +0200

    Refactor state enumeration values to uppercase.

--- app/utils/state_enum.py
+++ app/utils/state_enum.py
@@ -6,6 +6,9 @@ class BatchState(Enum):
     Represents the different states of a batch.
     """
 
-    ACTIVE = "Active"
-    COMPLETED = "Completed"
-    ERROR = "Error"
+    ACTIVE = "ACTIVE"
+    COMPLETED = "COMPLETED"
+    ERROR = "ERROR"
+
+    def __str__(self) -> str:
+        return self.value
