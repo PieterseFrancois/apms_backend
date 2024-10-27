5ff217976fa4ed1cea8fe9488b1016426a204a5b
Francois Pieterse <100724847+PieterseFrancois@users.noreply.github.com>
2024-10-27 20:06:41 +0200

    Included temperature profile endpoints.

--- app/routers/oven.py
+++ app/routers/oven.py
@@ -6,7 +6,7 @@ from app.dependencies import get_db
 from app.utils.http_messages import HTTPMessages
 from app.utils.message_identifiers import MessageIdentifiers
 from app.utils.state_enum import BatchState
-from app.utils.logs_enums import OvenLogDescription, OvenLogType
+from app.utils.logs_enums import OvenLogType
 
 from app.schemas import (
     Response,
@@ -15,10 +15,13 @@ from app.schemas import (
     TemperatureLogBase,
     TemperatureLogCreate,
     TemperatureLog,
-    OvenLogBase,
     OvenLogCreate,
     OvenLog,
     OvenLogExpanded,
+    TemperatureProfileBase,
+    TemperatureProfileCreate,
+    TemperatureProfile,
+    Machine,
 )
 
 from app.crud.oven import (
@@ -31,6 +34,14 @@ from app.crud.oven import (
     get_temperature_logs_for_machine,
     create_log,
     get_logs_for_machine,
+    create_temperature_profile,
+    get_temperature_profiles_for_machine,
+    delete_temperature_profile,
+    get_active_temperature_profile_for_machine,
+)
+
+from app.crud.machines import (
+    set_machine_active_temperature_profile,
 )
 
 from datetime import datetime, timezone
@@ -361,9 +372,7 @@ async def create_log_route(
         created_log_dict, machine_id, MessageIdentifiers.OvenLog
     )
 
-    return Response(
-        success=True, msg=HTTPMessages.OVEN_LOG_CREATED, data=[created_log]
-    )
+    return Response(success=True, msg=HTTPMessages.OVEN_LOG_CREATED, data=[created_log])
 
 
 @router.get("/oven/logs/{machine_id}", response_model=Response, tags=["Oven - Log"])
@@ -398,4 +407,187 @@ def get_logs_for_machine_route(
 
     return Response(
         success=True, msg=HTTPMessages.OVEN_LOGS_RETRIEVED, data=expanded_logs
+    )
+
+
+@router.post(
+    "/oven/profile/{machine_id}", response_model=Response, tags=["Oven - Profile"]
+)
+def create_temperature_profile_route(
+    machine_id: int,
+    temperature_profile: TemperatureProfileBase,
+    db: Session = Depends(get_db),
+) -> Response:
+    """
+    Creates a temperature profile for the oven.
+
+    Args:
+        machine_id (int): The machine identifier.
+        temperature_profile (TemperatureProfileBase): The temperature profile.
+        db (Session): The database session.
+
+    Returns:
+        Response: The response containing the temperature profile.
+    """
+
+    new_temperature_profile = TemperatureProfileCreate(
+        label=temperature_profile.label,
+        max_temp=temperature_profile.max_temp,
+        safe_temp=temperature_profile.safe_temp,
+        desired_temp=temperature_profile.desired_temp,
+        bake_time_sec=temperature_profile.bake_time_sec,
+        machine_id=machine_id,
+    )
+
+    temperature_profile: TemperatureProfileCreate = create_temperature_profile(
+        db, new_temperature_profile
+    )
+
+    return Response(
+        success=True,
+        msg=HTTPMessages.TEMPERATURE_PROFILE_CREATED,
+        data=[temperature_profile],
+    )
+
+
+@router.get(
+    "/oven/profiles/{machine_id}", response_model=Response, tags=["Oven - Profile"]
+)
+def get_temperature_profiles_for_machine_route(
+    machine_id: int,
+    db: Session = Depends(get_db),
+) -> Response:
+    """
+    Retrieves the temperature profiles for the oven based on the machine identifier.
+
+    Args:
+        machine_id (int): The machine identifier.
+        db (Session): The database session.
+
+    Returns:
+        Response: The response containing the temperature profiles.
+    """
+
+    temperature_profiles: list[TemperatureProfile] = (
+        get_temperature_profiles_for_machine(db, machine_id)
+    )
+
+    return Response(
+        success=True,
+        msg=HTTPMessages.TEMPERATURE_PROFILES_RETRIEVED,
+        data=temperature_profiles,
+    )
+
+
+@router.delete(
+    "/oven/profile/{profile_id}", response_model=Response, tags=["Oven - Profile"]
+)
+def delete_temperature_profile_route(
+    profile_id: int,
+    db: Session = Depends(get_db),
+) -> Response:
+    """
+    Deletes a temperature profile for the oven.
+
+    Args:
+        profile_id (int): The profile identifier.
+        db (Session): The database session.
+
+    Returns:
+        Response: The response containing the temperature profile.
+    """
+
+    delete_temperature_profile(db, profile_id)
+
+    return Response(
+        success=True,
+        msg=HTTPMessages.TEMPERATURE_PROFILE_DELETED,
+        data=[],
+    )
+
+
+@router.get(
+    "/oven/{machine_id}/profile/set-active/{profile_id}",
+    response_model=Response,
+    tags=["Oven - Profile Active"],
+)
+async def set_machine_active_temperature_profile_route(
+    machine_id: int,
+    profile_id: int,
+    db: Session = Depends(get_db),
+) -> Response:
+    """
+    Sets the active temperature profile for a machine.
+
+    Args:
+        machine_id (int): The machine ID.
+        profile_id (int): The profile ID.
+        db (Session): The database session.
+
+    Returns:
+        Response: The response containing the updated machine.
+    """
+
+    updated_machine_orm = set_machine_active_temperature_profile(
+        db, machine_id, profile_id
+    )
+
+    updated_machine: Machine = Machine(
+        id=updated_machine_orm.id,
+        name=updated_machine_orm.name,
+        active_profile_id=updated_machine_orm.active_profile_id
+    )
+
+    # Send the active profile to the machine
+    try:
+        await WebSocketManager.send_personal_message(
+            updated_machine.dict(), machine_id, MessageIdentifiers.ActiveProfile
+        )
+    except Exception:
+        # Create log entry
+        # Placeholder for the actual implementation.
+        pass
+
+    return Response(
+        success=True,
+        msg=HTTPMessages.MACHINE_ACTIVE_PROFILE_SET,
+        data=[updated_machine],
+    )
+
+
+@router.get(
+    "/oven/{machine_id}/profile/get-active",
+    response_model=Response,
+    tags=["Oven - Profile Active"],
+)
+def get_active_temperature_profile_for_machine_route(
+    machine_id: int,
+    db: Session = Depends(get_db),
+) -> Response:
+    """
+    Retrieves the active temperature profile for the oven based on the machine identifier.
+
+    Args:
+        machine_id (int): The machine identifier.
+        db (Session): The database session.
+
+    Returns:
+        Response: The response containing the temperature profile.
+    """
+
+    temperature_profile: TemperatureProfile | None = (
+        get_active_temperature_profile_for_machine(db, machine_id)
+    )
+
+    if temperature_profile is None:
+        return Response(
+            success=False,
+            msg=HTTPMessages.NO_TEMPERATURE_PROFILES_FOUND,
+            data=[],
+        )
+
+    return Response(
+        success=True,
+        msg=HTTPMessages.ACTIVE_TEMPERATURE_PROFILE_RETRIEVED,
+        data=[temperature_profile],
     )
