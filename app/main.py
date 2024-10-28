8ebea25f29bba7cd38e7e22949a1a47658b08434
Francois Pieterse <100724847+PieterseFrancois@users.noreply.github.com>
2024-10-28 17:16:07 +0200

    Fixed bug in websocket connection.

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ async def websocket_endpoint(websocket: WebSocket, client_id: str) -> None:
         while True:
             await websocket.receive_text()
     except Exception:
-        WebSocketManager.disconnect(client_id)
+        WebSocketManager.disconnect(client_id, websocket)
 
 
 @app.exception_handler(RequestValidationError)
